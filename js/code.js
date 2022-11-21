a062d71eb22f8f06a6269db0ee9435f099953026
Reece1109 <reecesimon84@gmail.com>
2022-11-21 11:07:55 +0200

    Update

---
+++ js/code.js
@@ -0,0 +1,5 @@
+let sentence =  "I am a Developer and I love coding";
+let index = 7;
+console.log(`charAt(${index}): ${sentence.charAt(index)}`);
+console.log(`includes(): ${sentence.includes('love')}`);
+console.log(sentence.length);
